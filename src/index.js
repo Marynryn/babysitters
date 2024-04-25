0d91c6d69807412692e0a956a65567791abd0c84
Marynryn <murmar95@gmail.com>
2024-04-25 20:08:31 +0300

    npm i

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App.jsx';
-import './index.css';
-import { Provider } from 'react-redux';
-import { persistor, store } from './store/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "components/App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { persistor, store } from "./store/store";
 
-import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
-import Loader from 'components/Loader/Loader';
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import Loader from "components/Loader/Loader";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/campers">
+      <BrowserRouter basename="/babysitters">
         <PersistGate persistor={persistor} loading={<Loader />}>
           <App />
         </PersistGate>
-      </BrowserRouter>{' '}
+      </BrowserRouter>{" "}
     </Provider>
   </React.StrictMode>
 );
